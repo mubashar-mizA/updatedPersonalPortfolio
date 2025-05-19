27bfb576dbd61a02e45f8ebefa471e18891dd01f
mubashar-mizA <mirzamubashar535@gmail.com>
2025-05-19 16:46:01 +0500

    updating src/components/projects.jsx

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -5,6 +5,65 @@ export default function Projects() {
 
         {/* ****************************************************************** */}
 
+        {/* ok */}
+
+        <div className="relative bg-gray-300 lg:flex justify-between
+         items-center  lg:gap-x-36 gap-y-10 lg:p-0 p-[5px] lg:pt-10 pt-5">
+
+          <div className="w-[2px] bg-white this-is-line left-1/2 hidden lg:block  absolute top-0 bottom-0"></div>
+
+          <div className="lg:m-4  lg:p-2 p-4 lg:w-1/2  relative bg-transparent  ">
+
+            <div className="relative">
+
+              <div className="hover:cursor-pointer hover:scale-105  transition-all duration-700  ease-in-out group ">
+                <a href="https://skool-synch-system.vercel.app/">
+                  <img
+                    src="skolSync.svg"
+                    alt="alpha"
+                    className="overflow-hidden"
+                  />
+                </a>
+
+                {/* Tooltip */}
+
+                <div className="opacity-0 group-hover:opacity-100 transition-all duration-150 absolute -top-2 left-1/2 transform -translate-x-1/2 -translate-y-full  text-sm rounded-lg hover:text-black">
+                  <a href="https://skool-synch-system.vercel.app/" className="hover:underline">
+                    Request. Manage. Approve.
+                  </a>
+                </div>
+              </div>
+
+              <div className="hidden lg:block  w-[1px] h-3/5 absolute bg-black 
+                top-0 right-0 rotate-90 translate-x-[50px] translate-y-[40px] transition-all duration-700">
+                <a href="https://skool-synch-system.vercel.app/" className="w-[20px] h-[20px] border border-gray-900 bg-blue-500 hover:bg-blue-600 rounded-full
+                 absolute -top-2 right-0 translate-x-[10px] 
+                translate-y-[1px] transition-all duration-700" />
+              </div>
+
+
+            </div>
+
+          </div>
+
+          <div className="lg:m-4 m-5 lg:p-4 p-4 lg:mr-7 lg:w-1/2  border-gray-100 lg:rounded-bl-[100px] lg:rounded-tr-[100px] hover:rounded-bl-[10px] hover:rounded-tr-[10px] hover:rounded-tl-[10px] hover:rounded-br-[10px] flex items-center bg-gradient-to-br from-white to-blue-300 text-gray-800  transition-all ease-in-out duration-1000 hover:cursor-pointer ">
+            <div className="text-gray-800 m-3 flex items-center justify-center flex-wrap">
+              <div className="">
+                <h1 className="text-xl text-blue-700 md:p-2 font-bold font-serif">Request. Manage. Approve.</h1>
+                <h6 className="text-sm font-serif sm:p-2">All in one place Simplified</h6>
+                <p className="sm:p-4">
+                  Skool Synch System by sharpedg.com helps organizations streamline the process of requesting school supplies like books, uniforms, and essentials. Built for speed, clarity, and ease of use.
+                </p>
+
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+
+
+        {/* ok */}
         <div className="relative bg-gray-300 lg:flex justify-between
          items-center flex-row-reverse lg:gap-x-36 gap-y-10 lg:p-0 p-[5px] lg:pt-10 pt-5">
 
@@ -15,22 +74,22 @@ export default function Projects() {
             <div className="relative">
 
               <div className="hover:cursor-pointer hover:scale-105  transition-all duration-700  ease-in-out group ">
-                <a href="https://todo-with-auth-mubashar-mizas-projects.vercel.app/">
-                  <img src="todoWebApp.svg" alt="alpha" className="" />
+                <a href="https://sharp-edg-notes.vercel.app/">
+                  <img src="sharpEdg.svg" alt="alpha" className="" />
                 </a>
 
                 {/* Tooltip */}
 
                 <div className="opacity-0 group-hover:opacity-100 transition-all duration-150 absolute -top-2 left-1/2 transform -translate-x-1/2 -translate-y-full  text-sm rounded-lg hover:text-black">
-                  <a href="https://todo-with-auth-mubashar-mizas-projects.vercel.app/" className="hover:underline">
-                    Make Your Todo
+                  <a href="https://sharp-edg-notes.vercel.app/" className="hover:underline">
+                    Ace Your Exams
                   </a>
                 </div>
               </div>
 
               <div className="hidden lg:block  w-[1px] h-3/5 absolute bg-black 
-                top-0 left-0 rotate-90 -translate-x-[30px] translate-y-[40px] transition-all duration-700">
-                <a href="https://todo-with-auth-mubashar-mizas-projects.vercel.app/" className="w-[20px] h-[20px] border border-gray-900 bg-gray-500 hover:bg-gray-600 rounded-full
+                top-0 left-0 rotate-90 -translate-x-[45px] translate-y-[40px] transition-all duration-700">
+                <a href="https://sharp-edg-notes.vercel.app/" className="w-[20px] h-[20px] border border-black bg-black  rounded-full
                  absolute bottom-0 right-0 translate-x-[9px] 
                 translate-y-[12px] transition-all duration-700" />
               </div>
@@ -40,14 +99,14 @@ export default function Projects() {
 
           </div>
 
-          <div className="lg:m-4 m-5 lg:p-4 p-4 lg:mr-7 lg:w-1/2  border-[#f0f8ff] lg:rounded-bl-[100px] lg:rounded-tr-[100px] hover:rounded-bl-[10px] hover:rounded-tr-[10px] hover:rounded-tl-[10px] hover:rounded-br-[10px] flex items-center  bg-[#0e867c] transition-all ease-in-out duration-1000 hover:cursor-pointer ">
+          <div className="lg:m-4 m-5 lg:p-4 p-4 lg:mr-7 lg:w-1/2  border-[#f0f8ff] lg:rounded-bl-[100px] lg:rounded-tr-[100px] hover:rounded-bl-[10px] hover:rounded-tr-[10px] hover:rounded-tl-[10px] hover:rounded-br-[10px] flex items-center  bg-gray-100 transition-all ease-in-out duration-1000 hover:cursor-pointer ">
             <div className="m-3 flex items-center justify-center flex-wrap">
               <div className="">
 
-                <h1 className="text-xl text-yellow-400 md:p-2 font-bold font-serif">A Responsive Todo WebApp</h1>
-                <h6 className="text-gray-50 text-sm font-serif md:p-2">Efficient Task Management Simplified</h6>
-                <p className="md:p-6 text-gray-100">
-                  This Todo app helps users manage tasks efficiently. Users can add, edit, mark as complete, or delete todos. It features separate sections for completed and deleted tasks. Built with Next.js and Tailwind CSS, the app uses APIs for seamless data handling, ensuring a dynamic and responsive user experience.
+                <h1 className="text-xl  md:p-2 font-bold font-serif text-blue-700">Ace Your Exams with Expert Notes</h1>
+                <h6 className="text-sm font-serif md:p-2">study materials designed to help you succeed in your exams.</h6>
+                <p className="md:p-6 ">
+                  Ace Your Exams" is an academic resource platform that provides students with expertly crafted notes and study materials. Designed to support exam preparation, the site offers clear, organized, and high-quality content to help learners achieve academic success efficiently
                 </p>
 
               </div>
@@ -58,10 +117,9 @@ export default function Projects() {
         </div>
 
 
-        {/* ****************************************************************** */}
-
+        {/*  */}
         <div className="relative bg-gray-300 lg:flex justify-between
-         items-center lg:gap-x-36 gap-y-10 lg:p-0 p-[5px] lg:pt-10 pt-5">
+         items-center  lg:gap-x-36 gap-y-10 lg:p-0 p-[5px] lg:pt-10 pt-5">
 
           <div className="w-[2px] bg-white left-1/2 hidden lg:block  absolute top-0 bottom-0"></div>
 
@@ -70,24 +128,24 @@ export default function Projects() {
             <div className="relative">
 
               <div className="hover:cursor-pointer hover:scale-105  transition-all duration-700  ease-in-out group ">
-                <a href="https://mdexplorecountries.netlify.app/">
-                  <img src="02-explore-countries.svg" alt="alpha" className="" />
+                <a href="https://forever-fro.vercel.app/">
+                  <img src="forever.svg" alt="alpha" className="" />
                 </a>
 
                 {/* Tooltip */}
 
                 <div className="opacity-0 group-hover:opacity-100 transition-all duration-150 absolute -top-2 left-1/2 transform -translate-x-1/2 -translate-y-full  text-sm rounded-lg hover:text-black">
-                  <a href="https://mdexplorecountries.netlify.app/" className="hover:underline">
-                    Explore Countries...
+                  <a href="https://forever-fro.vercel.app/" className="hover:underline">
+                    Forever  Elegant Fashion
                   </a>
                 </div>
               </div>
 
               <div className="hidden lg:block  w-[1px] h-3/5 absolute bg-black 
-                top-0 right-0 rotate-90 -translate-x-[px] translate-y-8 translate-x-[45px]  transition-all duration-700">
-                <a href="https://mdexplorecountries.netlify.app/" className="w-[20px] h-[20px] border border-gray-900 bg-gray-500 hover:bg-gray-600 rounded-full
-                 absolute top-0 right-0 translate-x-[9px] 
-                translate-y-[0px] transition-all duration-700" />
+                top-0 -right-[54px] rotate-90 translate-x-[0px] translate-y-[40px] transition-all duration-700">
+                <a href="https://forever-fro.vercel.app/" className="w-[20px] h-[20px] border border-pink-900 bg-pink-500 hover:bg-pink-600 rounded-full
+                 absolute -top-1 right-0 translate-x-[10px] 
+                translate-y-[1px] transition-all duration-700" />
               </div>
 
 
@@ -95,14 +153,14 @@ export default function Projects() {
 
           </div>
 
-          <div className="lg:m-4 m-5 lg:p-2 p-4 lg:mr-7 lg:w-1/2  border-[#f0f8ff] lg:rounded-bl-[100px] lg:rounded-tr-[100px]            hover:rounded-bl-[10px] hover:rounded-tr-[10px] hover:rounded-tl-[10px] hover:rounded-br-[10px] flex items-center  bg-[#fff] transition-all ease-in-out duration-1000 hover:cursor-pointer ">
+          <div className="lg:m-4 m-5 lg:p-4 p-4 lg:mr-7 lg:w-1/2  border-[#f0f8ff] lg:rounded-bl-[100px] lg:rounded-tr-[100px] hover:rounded-bl-[10px] hover:rounded-tr-[10px] hover:rounded-tl-[10px] hover:rounded-br-[10px] flex items-center  bg-white transition-all ease-in-out duration-1000 hover:cursor-pointer ">
             <div className="m-3 flex items-center justify-center flex-wrap">
               <div className="">
 
-                <h1 className="text-xl text-gray-800 font-bold font-serif md:p-2">Discover Countries Globally</h1>
-                <h6 className="text-gray-400 text-sm font-serif md:p-2">Search, filter, and explore countries based on name and region.</h6>
-                <p className="md:p-6">
-                  This React application allows users to explore global countries. It provides a search feature to find countries by name and a filter option to sort by region, such as Asia, Africa, or Europe. Each country displays its flag, region, capital, and population. Built using React, Tailwind CSS, and dynamic routing.
+                <h1 className="text-xl md:p-2 font-bold font-serif text-pink-500">Forever  Elegant Fashion Storefront</h1>
+                <h6 className=" text-sm font-serif md:p-2 text-pink-400">Discover the Latest in Fashion Trends</h6>
+                <p className="md:px-6 ">
+                  Forever is a modern, responsive fashion e-commerce website that showcases the latest arrivals in style. With a clean UI and intuitive navigation, users can explore featured collections, browse product categories, and access a dedicated admin panel. The site is fully functional and deployed on Vercel, combining sleek design with practical performance for an optimal shopping experience.
                 </p>
 
               </div>
@@ -111,6 +169,9 @@ export default function Projects() {
 
 
         </div>
+
+
+
 
         {/* ****************************************************************** */}
 
