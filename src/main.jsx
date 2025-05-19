0b42f6e527493f3df2e6cb674d2073230ddee5cd
mubashar-mizA <mirzamubashar535@gmail.com>
2025-05-19 16:45:46 +0500

    updating src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Landing from './components/landing.jsx';
-import Projects from './components/Projects.jsx';
+import Projects from './components/projects.jsx';
 import Footer from './components/footer.jsx';
 
 
